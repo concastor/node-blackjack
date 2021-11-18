3a57c71d61b7d62d727bf2a349d3606e7f844429
jake <jake.ejrichardson@gmail.com>
2021-11-17 21:58:23 -0500

    add ai player to game

--- scripts/blackjack.client.js
+++ scripts/blackjack.client.js
@@ -1,157 +1,166 @@
-
 var App = {}
 
 App.deal = function () {
-    App.socket.emit('deal');
+	App.socket.emit("deal")
 }
 
 App.hit = function () {
-    App.socket.emit('hit');
+	App.socket.emit("hit")
 }
 
 App.stand = function () {
-    App.socket.emit('stand');
+	App.socket.emit("stand")
 }
 
 App.getSuitHtml = function (suit) {
-    var image = 'club.png';
-    if (suit === 'H') {
-        image = 'heart.png';
-    } else if (suit === 'S') {
-        image = 'spade.png';
-    } else if (suit === 'D') {
-        image = 'diamond.png';
-    }
-    return "<img class='card' src='img/" + image + "'/>";
+	var image = "club.png"
+	if (suit === "H") {
+		image = "heart.png"
+	} else if (suit === "S") {
+		image = "spade.png"
+	} else if (suit === "D") {
+		image = "diamond.png"
+	}
+	return "<img class='card' src='img/" + image + "'/>"
 }
 
 App.getRankHtml = function (rank) {
-    if (rank === 1) {
-        return 'A';
-    } else if (rank === 11) {
-        return 'J';
-    } else if (rank === 12) {
-        return 'Q';
-    } else if (rank === 13) {
-        return 'K';
-    }
-    return rank;
+	if (rank === 1) {
+		return "A"
+	} else if (rank === 11) {
+		return "J"
+	} else if (rank === 12) {
+		return "Q"
+	} else if (rank === 13) {
+		return "K"
+	}
+	return rank
 }
 
 App.getCardsHtml = function (cards) {
-    var html = '';
-    for (var i = 0; i < cards.length; i++) {
-        var card = cards[i];
-        html += App.getRankHtml(card.rank);
-        html += App.getSuitHtml(card.suit);
-    }
-    return html;
+	var html = ""
+	for (var i = 0; i < cards.length; i++) {
+		var card = cards[i]
+		html += App.getRankHtml(card.rank)
+		html += App.getSuitHtml(card.suit)
+	}
+	return html
 }
 
 App.updatePlayer = function (player) {
-    var html = App.getCardsHtml(player.cards);
-    $('#playerCards').html(html);
-    $('#playerScore').text(player.score);
+	var html = App.getCardsHtml(player.cards)
+	$("#playerCards").html(html)
+	$("#playerScore").text(player.score)
+}
+
+App.updateAI = function (player) {
+	var html = App.getCardsHtml(player.cards)
+	$("#AIcards").html(html)
+	$("#aiScore").text(player.score)
 }
 
 App.updateDealer = function (dealer) {
-    var html = App.getCardsHtml(dealer.cards);
-    $('#dealerCards').html(html);
-    $('#dealerScore').text(dealer.score);
+	var html = App.getCardsHtml(dealer.cards)
+	$("#dealerCards").html(html)
+	$("#dealerScore").text(dealer.score)
 }
 
 App.updateResult = function (result) {
-    var displayResult = result;
-    if (result === 'None') {
-        displayResult = '';
-    }
-    $('#result').text(displayResult);
+	var displayResult = result
+	if (result === "None") {
+		displayResult = ""
+	}
+	$("#result").text(displayResult)
 }
 
 App.disableButton = function (id) {
-    $(id).attr('disabled', 'disabled');
+	$(id).attr("disabled", "disabled")
 }
 
 App.enableButton = function (id) {
-    $(id).removeAttr('disabled');
+	$(id).removeAttr("disabled")
 }
 
 App.disableDeal = function () {
-    App.disableButton('#deal');
-    App.enableButton('#hit');
-    App.enableButton('#stand');
+	App.disableButton("#deal")
+	App.enableButton("#hit")
+	App.enableButton("#stand")
 }
 
 App.enableDeal = function () {
-    App.enableButton('#deal');
-    App.disableButton('#hit');
-    App.disableButton('#stand');
+	App.enableButton("#deal")
+	App.disableButton("#hit")
+	App.disableButton("#stand")
 }
 
 App.enableDealIfGameFinished = function (result) {
-    if (result !== 'None') {
-        App.enableDeal();
-    }
+	if (result !== "None") {
+		App.enableDeal()
+	}
 }
 
 App.dealResult = function (game) {
-    App.disableDeal();
-    App.updateDealer(game.dealer);
-    App.updatePlayer(game.player);
-    App.updateResult(game.result);
+	App.disableDeal()
+	App.updateDealer(game.dealer)
+	App.updatePlayer(game.player)
+	App.updateAI(game.ai_player)
+	App.updateResult(game.result)
 }
 
 App.hitResult = function (game) {
-    App.updateDealer(game.dealer);
-    App.updatePlayer(game.player);
-    App.updateResult(game.result);
-    App.enableDealIfGameFinished(game.result);
+	App.updateDealer(game.dealer)
+	App.updatePlayer(game.player)
+	App.updateResult(game.result)
+	App.updateAI(game.ai_player)
+
+	App.enableDealIfGameFinished(game.result)
 }
 
 App.standResult = function (game) {
-    App.updateDealer(game.dealer);
-    App.updatePlayer(game.player);
-    App.updateResult(game.result);
-    App.enableDealIfGameFinished(game.result);
+	App.updateDealer(game.dealer)
+	App.updatePlayer(game.player)
+	App.updateResult(game.result)
+	App.updateAI(game.ai_player)
+
+	App.enableDealIfGameFinished(game.result)
 }
 
 App.socket = {}
 
 App.registerClientActions = function () {
-    
-    $('#deal').click(function () {
-        App.deal();
-    });
+	$("#deal").click(function () {
+		App.deal()
+	})
 
-    $('#hit').click(function () {
-        App.hit();
-    });
+	$("#hit").click(function () {
+		App.hit()
+	})
 
-    $('#stand').click(function () {
-        App.stand();
-    });
+	$("#stand").click(function () {
+		App.stand()
+	})
 }
 
-App.registerServerActions = function () {    
-    App.socket.on('stand', function (game) {
-        App.standResult(game);
-    });
-    App.socket.on('deal', function (game) {
-        App.dealResult(game);
-    });
-    App.socket.on('hit', function (game) {
-        App.hitResult(game);
-    });
+App.registerServerActions = function () {
+	App.socket.on("stand", function (game) {
+		App.standResult(game)
+	})
+	App.socket.on("deal", function (game) {
+		App.dealResult(game)
+	})
+	App.socket.on("hit", function (game) {
+		App.hitResult(game)
+	})
 }
 
 App.init = function () {
-    var socket = io.connect('http://localhost:3000');
-    App.socket = socket;
-    App.registerClientActions();
-    App.registerServerActions();
-    App.enableDeal();
+	var socket = io.connect("http://localhost:3000")
+	App.socket = socket
+	App.registerClientActions()
+	App.registerServerActions()
+	App.enableDeal()
 }
 
 $(document).ready(function () {
-    App.init();
-});
+	App.init()
+})
